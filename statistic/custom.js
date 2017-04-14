b06063be4ed1f5ec42af301de0e2d409abfeef20
manxueitp <mw3060@nyu.edu>
2017-04-13 17:08:44 -0700

    show user current location

--- statistic/custom.js
+++ statistic/custom.js
@@ -2,17 +2,20 @@
 // - custom marker
 // - custom info window (using Info Bubble)
 // - custom info window content (+ store hours)
-
 var ICON = new google.maps.MarkerImage('../img/olababy.png', null, null,
     new google.maps.Point(14, 13));
 
-var SHADOW = new google.maps.MarkerImage('../img/pinshadow.png', null, null,
+var SHADOW = new google.maps.MarkerImage('medicare-shadow.png', null, null,
     new google.maps.Point(14, 13));
 
 google.maps.event.addDomListener(window, 'load', function() {
+  getLatlng();
+});
+
+var rendermap = function(latlng){
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(-28, 135),
-    zoom: 4,
+    center: new google.maps.LatLng(latlng.lat, latlng.lng),
+    zoom: 10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
@@ -38,4 +41,27 @@ google.maps.event.addDomListener(window, 'load', function() {
   new storeLocator.Panel(panelDiv, {
     view: view
   });
-});
+}
+
+var getLatlng=function(){
+  if(navigator.geolocation){
+    navigator.geolocation.watchPosition(successCallback, errorCallback, {});
+    function successCallback(currentPosition) {
+      var lat = currentPosition.coords.latitude,
+      long = currentPosition.coords.longitude;
+
+      var latlng={lat: lat, lng: long};
+      console.log(latlng);
+      rendermap(latlng);
+      }
+    function errorCallback(e) {
+      alert(e);
+      var latlng = {lat:45, lng:122};
+      rendermap(latlng);
+    }
+  } else {
+    alert("Geolocation is not supported by this browser.");
+    var latlng = {lat:45, lng:122};
+    rendermap(latlng);
+  }
+}
